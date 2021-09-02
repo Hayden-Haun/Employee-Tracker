46932e27aa631e6540e4ccc5fb74eb4f09c67ae9
Hayden-Haun <haydenhaun@gmail.com>
2021-09-01 23:04:53 -0600

    All functions link correctly to db/index.js

--- index.js
+++ index.js
@@ -53,23 +53,22 @@ async function mainMenu() {
     },
   ]);
 
-  console.log(mainMenuAction);
   switch (mainMenuAction) {
     case "View all departments":
       return viewDepartments();
-    case "view_employees":
-      return viewEmployees();
-    case "view_roles":
+    case "View all roles":
       return viewRoles();
-    case "add_department":
+    case "View all employees":
+      return viewEmployees();
+    case "Add a department":
       return addDepartment();
-    case "add_role":
+    case "Add a role":
       return addRole();
-    case "add_employee":
+    case "Add an employee":
       return addEmployee();
-    case "update_role":
+    case "Update an employee role":
       return updateRole();
-    case "quit":
+    case "QUIT application":
       return quit();
   }
 }
@@ -78,7 +77,7 @@ mainMenu();
 
 async function viewDepartments() {
   console.log("\n");
-  console.log("DEPARTMENTS");
+  console.log("ALL DEPARTMENTS");
 
   await db.findAllDepartments();
 
@@ -87,23 +86,27 @@ async function viewDepartments() {
   mainMenu();
 }
 
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-async function viewEmployees() {
-  const employees = await db.findAllEmployees();
-  console.log("\n");
-  console.table(employees);
-
-  mainMenu();
-}
-
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 async function viewRoles() {
-  const roles = await db.findAllRoles();
+  console.log("\n");
+  console.log("ALL ROLES");
+
+  await db.findAllRoles();
 
   console.log("\n");
-  console.table(roles);
+
+  mainMenu();
+}
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+async function viewEmployees() {
+  console.log("\n");
+  console.log("ALL EMPLOYEES:");
+
+  await db.findAllEmployees();
+
+  console.log("\n");
 
   mainMenu();
 }
@@ -111,28 +114,56 @@ async function viewRoles() {
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
 async function addDepartment() {
-  console.log("this is working!");
+  console.log("\n");
+  console.log("Add a Department");
+
+  await db.addDepartment();
+
+  console.log("\n");
+
+  mainMenu();
 }
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 async function addRole() {
-  console.log("this is working!");
+  console.log("\n");
+  console.log("Add a role");
+
+  await db.findAllRoles();
+
+  console.log("\n");
+
+  mainMenu();
 }
 
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 async function addEmployee() {
-  console.log("this is working!");
+  console.log("\n");
+  console.log("Add an employee");
+
+  await db.findAllRoles();
+
+  console.log("\n");
+
+  mainMenu();
 }
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 async function updateRole() {
-  console.log("this is working!");
+  console.log("\n");
+  console.log("Update an employee role");
+
+  await db.findAllRoles();
+
+  console.log("\n");
+
+  mainMenu();
 }
 
 //Quits application
 async function quit() {
-  console.log("quit this is working!");
+  console.log("QUITTING APPLICATION");
 }
